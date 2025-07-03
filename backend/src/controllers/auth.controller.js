b0194fe8c095ca01378ed42dc2112d876185548f
Deepak kumar <avinash96937@gmail.com>
2025-07-03 13:06:46 +0530

    complite auth routes and also middleware for protected route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -9,7 +10,9 @@ export const signup = async (req, res) => {
     }
 
     if (password.length < 6) {
-      return res.status(400).json({ message: "Password must be at least 6 characters long" });
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -20,7 +23,9 @@ export const signup = async (req, res) => {
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "Email already exists, please use a different one" });
+      return res
+        .status(400)
+        .json({ message: "Email already exists, please use a different one" });
     }
 
     const idx = Math.floor(Math.random() * 100) + 1;
@@ -33,11 +38,23 @@ export const signup = async (req, res) => {
       profilePic: randomAvatar,
     });
 
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullName,
+        image: newUser.profilePic || "",
+      });
+      console.log(`Stream user created for ${newUser.fullName}`);
+    } catch (error) {
+      console.log("Error creating Stream user:", error);
+    }
 
-    // TODO: Create the user in stream as well
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "3d" }
+    );
 
-    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: '3d' });
-   
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
@@ -45,13 +62,17 @@ export const signup = async (req, res) => {
       maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
     });
 
-    res.status(201).json({ success: true, message: "User created successfully", user: newUser });
-
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "User created successfully",
+        user: newUser,
+      });
   } catch (error) {
     console.error("Error in signup controller :", error);
     res.status(500).json({ message: "Internal server error" });
   }
-
 };
 
 export const login = async (req, res) => {
@@ -60,16 +81,19 @@ export const login = async (req, res) => {
     if (!email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-    
-    const user = await User.findOne({ email });
-    if(!user) return res.status(400).json({ message: "Invalid email or password" });
 
-    const isPasswordValid = await user.matchPassword(password)
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const isPasswordValid = await user.matchPassword(password);
     if (!isPasswordValid) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '3d' });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "3d",
+    });
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
@@ -77,8 +101,7 @@ export const login = async (req, res) => {
       maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
     });
 
-    res.status(200).json({success:true, user})
-
+    res.status(200).json({ success: true, user });
   } catch (error) {
     console.error("Error in login controller:", error);
     res.status(500).json({ message: "Internal server error" });
@@ -87,5 +110,58 @@ export const login = async (req, res) => {
 
 export async function logout(req, res) {
   res.clearCookie("jwt");
-  res.status(200).json({ success: true, message: "Logged out successfully" });  
+  res.status(200).json({ success: true, message: "Logged out successfully" });
+}
+
+export async function onboard(req, res) {
+  try {
+    const userId = req.user._id;
+
+    const { fullName, bio, nativeLanguage, learningLanguage, location } =
+      req.body;
+
+    if (
+      !fullName ||
+      !bio ||
+      !nativeLanguage ||
+      !learningLanguage ||
+      !location
+    ) {
+      return res.status(400).json({
+        message: "All fields are required",
+        missingFields: [
+          !fullName && "fullName",
+          !bio && "bio",
+          !nativeLanguage && "nativeLanguage",
+          !learningLanguage && "learningLanguage",
+          !location && "location",
+        ].filter(Boolean),
+      });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, {
+      ...req.body,
+      isOnboarded: true,
+    }, { new: true })
+
+    if (!updatedUser) return res.status(404).json({ message: "User not found" });
+
+    try {
+      await upsertStreamUser({
+        id: updatedUser._id.toString(),
+        name: updatedUser.fullName,
+        image: updatedUser.profilePic || "", // fallback to empty string if no image
+      });
+
+      console.log("✅ Stream user updated after onboarding for:", updatedUser.fullName);
+    } catch (error) {
+      console.log("Error updating Stream user after onboarding:", error.message);
+    }
+
+    res.status(200).json({ success: true, user: updatedUser })
+
+  } catch (error) {
+    console.error("Error in onboard controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,17 +2,20 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the backend server!");
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
