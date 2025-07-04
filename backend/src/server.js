d67ec134389f4894880ec2b384b786e4d0c60548
Deepak kumar <avinash96937@gmail.com>
2025-07-04 12:05:36 +0530

    complite the user route and controller function

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
