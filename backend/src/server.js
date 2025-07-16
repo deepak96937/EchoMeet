10cb408dd720ca49253c6648e2e0d46706c9da76
Deepak kumar <avinash96937@gmail.com>
2025-07-16 15:32:32 +0530

    chat and user route completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import "dotenv/config"
 import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser"
 
@@ -14,6 +16,8 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/chat", chatRoutes)
 
 
 app.listen(PORT, () => {
