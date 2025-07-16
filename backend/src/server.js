ca996a3b878e99e12f4a3965e0bf529f5f948175
Deepak kumar <avinash96937@gmail.com>
2025-07-16 13:45:08 +0530

    fixed error in auth controller file and add one more features for checking user is authincated or not

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import "dotenv/config"
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from './lib/db.js';
+import cookieParser from "cookie-parser"
 
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
