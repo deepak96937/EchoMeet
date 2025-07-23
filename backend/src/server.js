6db0cc48b51ee73ba35aba3bef26b7277366137a
Deepak kumar <avinash96937@gmail.com>
2025-07-23 13:29:59 +0530

    add all component and page

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,17 @@ import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser"
+import cors from 'cors';
+
+
 
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173", // Allow requests from the frontend
+    credentials: true, // Allow credentials to be sent
+}));
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
